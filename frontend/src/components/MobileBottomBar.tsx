5d74e7a227eb92a362de18ec8e4cd8fda812cc84
Pace Zhou <pacezhou@gmail.com>
2026-03-06 18:22:25 -0800

    feat: Change mobile bars to mango blue (#E3F2FF)

--- frontend/src/components/MobileBottomBar.tsx
+++ frontend/src/components/MobileBottomBar.tsx
@@ -11,7 +11,7 @@ export default function MobileBottomBar() {
   const isActive = (path: string) => location.pathname === path
 
   return (
-    <div className="fixed bottom-0 left-0 right-0 z-50 bg-[#E5F2B3] border-t border-gray-200 md:hidden">
+    <div className="fixed bottom-0 left-0 right-0 z-50 bg-[#E3F2FF] border-t border-gray-200 md:hidden">
       <div className="flex items-center justify-around h-14">
         {/* 芒一下 */}
         <Link to="/" className="flex flex-col items-center justify-center flex-1 h-full">
